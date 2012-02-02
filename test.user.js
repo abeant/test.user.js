0f3c7b131bf893264c081f73bf3ca55cb0aabb4b
abeant <abrahamantigua@gmail.com>
2012-02-02 15:22:59 -0800

    first commit

---
+++ test.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name          Test Script
+// @namespace     test
+// @version       1.0
+// @date          2012-01-16
+// @author        Abraham Antigua
+// @description   console.log('test')
+// @include       *
+// ==UserScript==
+
+console.log('test')
